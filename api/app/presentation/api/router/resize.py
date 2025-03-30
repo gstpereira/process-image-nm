e5eadaa876fe0fbc8c9fb34f14b026c7ef376af0
Gustavo Pereira <guustavo0.henrique@gmail.com>
2025-03-30 17:01:56 -0300

    Add valition to limit to width and height

--- api/app/presentation/api/router/resize.py
+++ api/app/presentation/api/router/resize.py
@@ -3,7 +3,7 @@ from app.domain.entity import UploadFile
 from app.application.use_case import ResizeFile
 from app.infra.container.container import Container
 from dependency_injector.wiring import Provide, inject
-from fastapi import APIRouter, UploadFile as FastUploadFile, Depends
+from fastapi import APIRouter, UploadFile as FastUploadFile, Depends, Query
 
 router = APIRouter(
     prefix="/resize",
@@ -14,8 +14,8 @@ router = APIRouter(
 async def create_upload_file(
     file: FastUploadFile,
     resize_file_use_case: Annotated[ResizeFile, Depends(Provide[Container.resize_file_use_case])],
-    width: int = 384,
-    height: int = 384):
+    width: int = Query(gt=0, lt=1201, default = 384),
+    height: int = Query(gt=0, lt=1201, default = 384)):
     upload_file = UploadFile(
         name=file.filename,
         width=width,
